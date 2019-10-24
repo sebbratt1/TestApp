78f5dc9f6312edcf38b09d89f3bb3bc0e037da82
sebbratt1 <43443754+sebbratt1@users.noreply.github.com>
2019-10-24 15:03:41 +0100

    Added content

---
+++ TestApp/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+// Local Storage - Storing
+
+var key;
+var value;
+var parseValue;
+var halfGalaxy;
+var halfGalaxyString;
+
+key = "orange";
+value = "orange";
+window.localStorage.setItem(key, value);
+
+key = "phone";
+value = "bad";
+window.localStorage.setItem(key, value);
+
+key = "cable";
+value = "got";
+window.localStorage.setItem(key, value);
+
+key = "hello";
+value = "world";
+window.localStorage.setItem(key, value);
+
+key = "key";
+value = "12345";
+window.localStorage.setItem(key, value);
+
+halfGalaxy = {
+	"energy": "2280kJ",
+	"fat": "32.4g",
+	"salt": "0.25g",
+	"protein": "6.7g",
+	"carb": "56.0g"
+}
+halfGalaxyString = JSON.stringify(halfGalaxy);
+key = "halfGalaxy";
+window.localStorage.setItem(key, halfGalaxyString);
+
+// Local Storage - Getting
+
+key = "phone";
+value = window.localStorage.getItem(key);
+window.alert(value);
+
+key = "halfGalaxy";
+value = window.localStorage.getItem(key);
+parseValue = JSON.parse(value);
+window.alert(parseValue.energy + parseValue.fat + parseValue.salt + parseValue.protein + parseValue.carb);
+
+window.alert(localStorage.length + " items stored in local storage");
+
+
+
+
+
